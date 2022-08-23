484db5b532a022927a08ba81359eef11448b81bd
Atulyw <atulyw@greamio.com>
2022-08-23 18:57:23 +0530

    atulyw | codedeploy added

--- code-deploy.sh
+++ code-deploy.sh
@@ -4,7 +4,7 @@ yum install -y ruby
 yum install -y aws-cli
 mkdir -p /tmp/codedeploy
 cd /tmp/codedeploy
-aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-west-2 
+aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-east-1 
 chmod +x ./install
 ./install auto
 service codedeploy-agent start
